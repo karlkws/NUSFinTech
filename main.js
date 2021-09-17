6f9ecb999a2fdfb4fc2aa0eeba4acadf6d2e7c30
Karl <karlkws95@gmail.com>
2021-09-17 14:29:37 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const data = require('./data'); 
+// To import diff file in same folder, use ./ notation. If package, just use package name directly
+// ./ means current folder, .. means folder above
+
+//Create server application
+server = express();
+server.use(express.json()); // Tell server to use posted data in JSON format
+
+
+// Create router object
+router = express.Router();
+
+// Define mappings
+// Map URI to URL
+
+router.get("/user/all", (request, response) => {
+    let users = data.get_all_users(); // get all the users
+    response.send(users);
+});
+
+router.get("/user/by-uid", (request, response) => {
+    let user_id = request.query.user_id;
+    let user = data.get_user_by_user_id(user_id); // get a user based on user_id
+    response.send(user);
+});
+
+router.post("/user/add", (request, response) => {
+    let user = request.body; // Step 1: get user object from request
+    data.add_user(user); // Step 2: add user 
+    response.send("Added succcesfully!"); 
+});
+
+
+router.get("/", (request, response) => {
+    // should have standard 2 args: request, response
+    response.send("Welcome to Dev Toolkit #2!");
+});
+
+// GET request
+router.get("/sum", (request, response) =>{
+    let x = parseFloat(request.query.x);
+    let y = parseFloat(request.query.y);
+    let sum = x+y;
+
+    response.send("Sum is: " + sum.toFixed(1));
+});
+
+// POST request
+router.post("/create", (request, response) =>{
+    let name = request.body.name; // use .body for POST instead of .query, so that input will not appear in the URI/URL, for security purpose
+    let phone = request.body.phone;
+    let age = request.body.age;
+
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+    console.log("Phone: " + phone);
+
+    response.send("Data received.")
+});
+
+
+
+
+// Tell server to use created router mapping
+server.use(router);
+
+// Start server
+// listen(server no, error) = Start server on port no., throw error message if any
+// Ctrl+C in terminal to stop/restart server after making changes to main.js file 
+server.listen(3000, (errors) => {  
+    if(errors) {
+        console.log("Server ecould not start. Error" + errors);
+    }
+    else {
+        console.log("Server started on port 3000");
+    }
+});
